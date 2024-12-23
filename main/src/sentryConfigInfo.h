942d385f9d060e02b00e687b69644471e22c83a5
itivoli <139672232+itivoli@users.noreply.github.com>
2024-12-23 03:14:22 -0500

    SentryConfig Changes

--- main/src/sentryConfigInfo.h
+++ main/src/sentryConfigInfo.h
@@ -1,3 +1,18 @@
-// Sensor pin defintiions.
+#define BAUD_RATE 115200
+#define ONE_SECOND 1000
 
-// Wi-Fi 
+// KC Wifi.
+//#define WIFI_SSID "WhiteSky-KnightsCircle"
+//#define WIFI_PASSWORD "d9wk6xfg"
+
+// Jax Wifi.
+#define WIFI_SSID "NETGEAR66"
+#define WIFI_PASSWORD "dynamicbug829"
+
+// Firebase authentications
+#define USER_EMAIL "es849112@ucf.edu"
+#define USER_PASSWORD "rCpnKBR4ZhtefmL"
+
+// Firebase RTDB Keys/Urls.
+#define API_KEY "AIzaSyAGnxeU6342_TcjpmTKPT_WjB4AVTODfMk"
+#define DATABASE_URL "https://seems-hub-default-rtdb.firebaseio.com/"
